77cbf493cf439ecbbc7ea8fd85b2b0da9a6e8ea2
Guilherme Souza <ghostgui60@gmail.com>
2023-07-30 19:14:36 -0300

    Create container file

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -9,9 +9,11 @@ namespace Framework;
 class App
 {
     private Router $router;
+    private Container $container;
     public function __construct()
     {
         $this->router = new Router();
+        $this->container = new Container();
     }
     public function run()
     {
